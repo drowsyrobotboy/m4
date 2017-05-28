08978ee54cd876ffea6b5837a3b42a3335119290
Maruthi Sharma <maruthisharma@live.in>
2017-05-28 13:37:32 +0530

    added Dynamic menu and contact color styles

--- app/components/menu.jsx
+++ app/components/menu.jsx
@@ -7,19 +7,22 @@ class Menu extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      open : false
+      open : false,
+      burgerStyle: this.props.styleDefs.burgerDefStyle // initializing to set the value passed from caller
     };
     this.toggleBurger = this.toggleBurger.bind(this); // binds the function to the component on initiation =>> allows to use state
   };
   toggleBurger(){
     if(!this.state.open){
       this.setState({
-        open : true
+        open : true,
+        burgerStyle: this.props.styleDefs.burgerOpenStyle
       });
     }
     else{
       this.setState({
-        open : false
+        open : false,
+        burgerStyle: this.props.styleDefs.burgerDefStyle
       });
     }
   };
@@ -27,18 +30,22 @@ class Menu extends React.Component {
     return(
       <div className="menu">
         <div className={this.state.open? 'burger open': 'burger'} onClick={this.toggleBurger}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span style={this.state.burgerStyle}></span>
+          <span style={this.state.burgerStyle}></span>
+          <span style={this.state.burgerStyle}></span>
+          <span style={this.state.burgerStyle}></span>
+          <span style={this.state.burgerStyle}></span>
+          <span style={this.state.burgerStyle}></span>
         </div>
-        <div className={this.state.open? 'nav open': 'nav'}>
+        <div className={this.state.open? 'nav open': 'nav'} style={this.props.styleDefs.menuBgStyle}>
           <ul>
             {
               this.props.items.map(function(item) {
-                return (<li key={item.id}><a href={item.url}>{item.text}</a></li>)
+                return (
+                  <li key={item.id}>
+                    <a href={item.url}>{item.text}</a>
+                  </li>
+                )
               })
             }
           </ul>
